cbdb2eabde5c5cb71c2b8a316f4dd644a0324379
jebyrnes <jarrett.byrnes@umb.edu>
2023-02-15 09:03:02 -0500

    Add more comparison to calibration source

--- scripts/5_plot_calibration_mur.R
+++ scripts/5_plot_calibration_mur.R
@@ -5,6 +5,7 @@
 library(readr)
 library(ggplot2); theme_set(theme_minimal(base_size = 12)) #for viz
 library(patchwork)
+library(dplyr)
 
 joined_dat <- read_csv("derived_data/joined_calibration_mur_data.csv")
 
@@ -37,3 +38,17 @@ log_fit <- ggplot(joined_dat,
 
 combined_plot <- linear_nofit + log_fit + plot_layout(guides = "collect")
 ggsave("figures/calibration_plot.jpg", combined_plot, width = 12)
+
+
+#### Show timeseries ####
+joined_dat |>
+  mutate(date = as.Date(paste(year, month, day, sep = "-"))) |>
+  tidyr::pivot_longer(c(temp_c_logger, temp_c_mur),
+                      values_to = "temp_c", names_to = "source") |>
+  ggplot(aes(x = datetime, y = temp_c, color = source)) +
+  geom_point(alpha = 0.5) +
+  facet_wrap(vars(site), scale = "free_x") +
+  scale_color_manual(values = c("blue", "red"),
+                     labels = c("Benthic Loggers", "MUR 4.1 SST")) +
+  labs(x = "", y = "Temperature (C)", color = "Data Source")
+ggsave("figures/mur_calibration_timeseries.jpg")
